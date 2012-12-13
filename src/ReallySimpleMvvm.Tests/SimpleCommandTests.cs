361d004c7ba40ebde902ba74c780cb9ec8aaec06
ChrisWay <chris@darksquare.net>
2012-12-13 18:12:35 +0000

    Added first test for SimpleCommand.

---
+++ src/ReallySimpleMvvm.Tests/SimpleCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReallySimpleMvvm.Tests
+{
+	[TestFixture]
+	public class SimpleCommandTests
+	{
+		[Test]
+		public void WhenNoCanExecuteDelegateIsPassedCanExecute_ShouldReturnTrue()
+		{
+			var command = new SimpleCommand(() => { });
+
+			bool result = command.CanExecute(null);
+
+			Assert.IsTrue(result,"Can Execute should always return true when no delgate is given.");
+		}
+	}
+}
