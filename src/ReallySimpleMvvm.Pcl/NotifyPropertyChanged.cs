bf6aacf61e0c94760d1ab6b7e6acb7d744ed22a4
ChrisWay <chris@darksquare.net>
2012-12-13 19:12:33 +0000

    Added NotifyPropertyChanged implementation.

--- src/ReallySimpleMvvm.Pcl/NotifyPropertyChanged.cs
+++ src/ReallySimpleMvvm.Pcl/NotifyPropertyChanged.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 
 namespace ReallySimpleMvvm.Pcl
 {
@@ -12,7 +10,48 @@ namespace ReallySimpleMvvm.Pcl
 
 		public void RaisePropertyChangedEvent(string propertyName)
 		{
-			
+			var handler = PropertyChanged;
+			if(handler != null)
+				handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public void RaisePropertyChangedEvent<T>(Expression<Func<T>> property)
+		{
+			RaisePropertyChangedEvent(PropertyNameHelper.GetPropertyName(property));
+		}
+	}
+
+	/// <summary>
+	/// Source: http://keith-woods.com/Blog/post/Strongly-Typed-INotifyPropertyChanged-Event-Raisers.aspx
+	/// </summary>
+	internal class PropertyNameHelper
+	{
+		public static string GetPropertyName<T>(Expression<Func<T>> expression)
+		{
+			return GetPropertyNameFromLambda(expression);
+		}
+
+		public static string GetPropertyName<T>(Expression<Func<T, Object>> expression)
+		{
+			return GetPropertyNameFromLambda(expression);
+		}
+
+		private static string GetPropertyNameFromLambda(LambdaExpression lambda)
+		{
+			MemberExpression memberExpression = null;
+			if (lambda.Body is UnaryExpression)
+			{
+				var unaryExpression = lambda.Body as UnaryExpression;
+				if (unaryExpression != null) 
+					memberExpression = unaryExpression.Operand as MemberExpression;
+			}
+			else
+				memberExpression = lambda.Body as MemberExpression;
+
+			if (memberExpression == null)
+				throw new ArgumentException(String.Format("Property expression '{0}' did not provide a property name.", lambda));
+
+			return memberExpression.Member.Name;
 		}
 	}
 }
