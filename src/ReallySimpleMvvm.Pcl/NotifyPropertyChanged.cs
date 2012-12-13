d0501ea8d27a2bb9a56315ffebbe4442be0b6898
ChrisWay <chris@darksquare.net>
2012-12-13 19:01:40 +0000

    Added NotifyPropertyChanged class.

---
+++ src/ReallySimpleMvvm.Pcl/NotifyPropertyChanged.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace ReallySimpleMvvm.Pcl
+{
+	public abstract class NotifyPropertyChanged : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void RaisePropertyChangedEvent(string propertyName)
+		{
+			
+		}
+	}
+}
